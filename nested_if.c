af871afb9944ee94d469dfc3dad0b824782554f9
Shagun-kumar01 <lakhan4773@gmail.com>
2026-05-06 11:12:56 +0530

    use_of_nested_if
    Added closing brace and output comment.

---
+++ nested_if.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() 
+{
+int a, b;
+a = 34;
+b = 45;
+
+if (a < b){
+if (b > 23) {
+printf(" B is very Bigger than 23 ");
+}
+}
+return 0;
+}
+
+/* Output is 
+
+B is very Bigger than 23
+
+*/
